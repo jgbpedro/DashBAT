bf3532d363ce6b3fa52aa28882ec2bc2cdd70e15
jgbpedro <jguilhermebp@live.com>
2019-08-09 17:44:06 -0300

    Import data from users

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,36 @@
-<template>
-  <h1 class="text-danger">Title</h1>
+<template lang="pug">
+  div.container
+    h1.text-secondary Users 
+      i.fa.fa-user
+    table.table.table-sripped
+      tr
+        th Nome
+        th E-mail
+        th Instagram
+        th Senha
+        th Youtube
+        th Deezer
+        th Spotify
+      tr(v-for="user of users")
+        td {{user.firstName}}
+        td {{user.email}}
+        td {{user.insta || '-'}}
+        td {{user.password}}
+        td.text-left
+          div Subscribe
+          div Liked
+          div Comment 
+          div Last Updated
+        td.text-left
+          div Subscribe
+          div Liked
+          div Comment 
+          div Last Updated
+        td.text-left
+          div Subscribe
+          div Liked
+          div Comment 
+          div Last Updated    
 </template>
 
 <script>
@@ -7,24 +38,75 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      users: [
+        {
+          firstName: "Guilherme",
+          lastName: "Pedro",
+          email: "jgbpedro@bol.com",
+          insta: "",
+          password: "000000",
+          analytics: {
+            youtube: {
+              subscribe: false,
+              likedAll: false,
+              comment: false
+            },
+            spotify: {
+              follow: false,
+              listen: {
+                qty: 0
+              },
+              relation: [],
+              lastUpdate: ""
+            },
+            deezer: {
+              follow: false,
+              listen: {
+                qty: 0
+              },
+              relation: [],
+              lastUpdate: ""
+            }
+          }
+        },
+        {
+          firstName: "José",
+          lastName: "Pedro",
+          email: "pedro@bol.com",
+          insta: "",
+          password: "123456",
+          analytics: {
+            youtube: {
+              subscribe: false,
+              likedAll: false,
+              comment: false
+            },
+            spotify: {
+              follow: false,
+              listen: {
+                qty: 0
+              },
+              relation: [],
+              lastUpdate: ""
+            },
+            deezer: {
+              follow: false,
+              listen: {
+                qty: 0
+              },
+              relation: [],
+              lastUpdate: ""
+            }
+          }
+        }
+      ]
+    };
   }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
